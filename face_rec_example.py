f17660fd1a926a61b2faaeef823ad347afeeb8b1
Pavel <player49@mail.ru>
2022-09-06 19:23:35 +0200

    try to update links

--- face_rec_example.py
+++ face_rec_example.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import face_recognition
 import cv2
 
@@ -8,8 +9,9 @@ UNKNOWN_FACES_DIR = "unknown_faces"
 
 TOLERANCE = 0.6     # default settings is 60%, but may vary
 
-FRAME_THICKNESS = 3
-FONT_THICKNESS = 2
+# FRAME_THICKNESS = 3
+# FONT_THICKNESS = 2
+
 MODEL = "cnn"   # however, hog also can be while running on cpu, but we use CUDA, right?
 
 print("loading known faces..")
@@ -17,56 +19,108 @@ print("loading known faces..")
 known_faces = []
 known_names = []
 
-for name in os.listdir(KNOWN_FACES_DIR):
-    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
 
-        # find image
-        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
-        # encode image features
-        encoding = face_recognition.face_encodings(image)[0]
 
-        known_faces.append(encoding)
-        known_names.append(name)
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+print("Starting Video")
+# reading the input using the camera
+result, image = cam.read()
 
-print("processing unknown pictures..")
+# If image will detected without any error,
+# show result
+if result:
 
-for filename in os.listdir(UNKNOWN_FACES_DIR):
+    # showing result, it take frame name and image
+    # output
+    # cv2.imshow("GeeksForGeeks", image)
 
-    print(filename)
-    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+    # saving image in local storage
+    # f"{UNKNOWN_FACES_DIR}/{filename}"
+    tempname = "temp.png"
 
-    locations = face_recognition.face_locations(image, model=MODEL)
+    #cv2.imwrite("temporary.png", image)
 
-    encoding = face_recognition.face_encodings(image, locations)
+    cv2.imwrite(f"{UNKNOWN_FACES_DIR}/{tempname}", image)
 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # If keyboard interrupt occurs, destroy image
+    # window
+    # cv2.waitKey(0)
+    # cv2.destroyWindow("GeeksForGeeks")
 
-    for face_encoding, face_location in zip(encoding, locations):
+# If captured image is corrupted, moving to else part
+else:
+    print("No image detected. Please, try again")
 
-        # main line that works lol, GIVES A LIST OF BOOLEANS
-        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
 
-        match = None
-        if True in results:
-            match = known_names[results.index(True)]
-            print(f"Matched with: {match}")
+def match_faces():
 
-            # opencv visualisation stuff, not needed already
+    # Looking throughout drivers library-
 
-            # top_left = (face_location[3], face_location[0])
-            # bottom_right = (face_location[1], face_location[2])
-            #
-            # color = [0, 255, 0]
-            #
-            # cv2.rectangle(image,top_left,bottom_right,color,FRAME_THICKNESS)
-            #
-            # top_left = (face_location[3], face_location[2])
-            # bottom_right = (face_location[1], face_location[2]+22)
-            # cv2.rectangle(image, top_left, bottom_right,color, cv2.FILLED)
-            # cv2.putText(image, (face_location[3]+10,face_location[2]+15), cv2.FONT_HERSHEY_PLAIN,
-            #             0.5, (200,200,200), FONT_THICKNESS)
+    for name in os.listdir(KNOWN_FACES_DIR):
+        for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
 
-    cv2.imshow(filename, image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            # find image
+            image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+            # encode image features
+            encoding = face_recognition.face_encodings(image)[0]
 
+            known_faces.append(encoding)
+            known_names.append(name)
+
+    print("processing unknown pictures..\n,,\n..\n,,")
+
+
+    # Matching unknown face with a library of known faces
+
+    for filename in os.listdir(UNKNOWN_FACES_DIR):
+
+        print("Looking into: " + filename + "\n")
+        image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+
+        locations = face_recognition.face_locations(image, model=MODEL)
+
+        encoding = face_recognition.face_encodings(image, locations)
+
+        #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        for face_encoding, face_location in zip(encoding, locations):
+
+            # main line that works lol, GIVES A LIST OF BOOLEANS
+            results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+
+            match = None
+            if True in results:
+                match = known_names[results.index(True)]
+                print(f"Matched with: {match}")
+                return match
+            else:
+                print("No matches")
+
+    os.remove(f"{UNKNOWN_FACES_DIR}/{tempname}")
+                # opencv visualisation stuff, not needed at all bcz runs on a server
+
+                # top_left = (face_location[3], face_location[0])
+                # bottom_right = (face_location[1], face_location[2])
+                #
+                # color = [0, 255, 0]
+                #
+                # cv2.rectangle(image,top_left,bottom_right,color,FRAME_THICKNESS)
+                #
+                # top_left = (face_location[3], face_location[2])
+                # bottom_right = (face_location[1], face_location[2]+22)
+                # cv2.rectangle(image, top_left, bottom_right,color, cv2.FILLED)
+                # cv2.putText(image, (face_location[3]+10,face_location[2]+15), cv2.FONT_HERSHEY_PLAIN,
+                #             0.5, (200,200,200), FONT_THICKNESS)
+
+        # cv2.imshow(filename, image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    match_faces()
